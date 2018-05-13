099214733b6cf2b6e24c0fd30b9fe61e2d00858b
8ar <jitash.cn@gmail.com>
2018-05-13 23:54:25 +0800

    p13

--- app/src/main/java/cn/jitash/exercise/android_advanced_light/chapter_1/notification/NofiticationActivity.java
+++ app/src/main/java/cn/jitash/exercise/android_advanced_light/chapter_1/notification/NofiticationActivity.java
@@ -1,17 +1,26 @@
 package cn.jitash.exercise.android_advanced_light.chapter_1.notification;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RemoteViews;
 
+import java.nio.channels.InterruptedByTimeoutException;
+
+import cn.jitash.exercise.MainActivity;
 import cn.jitash.exercise.R;
 
 public class NofiticationActivity extends AppCompatActivity {
@@ -22,18 +31,75 @@ public class NofiticationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nofitication);
         mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String CHANNEL_ID = "my_channel_01";
+            CharSequence name = "my_channel";
+            String description = "this is my channel";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            mNotificationManager.createNotificationChannel(mChannel);
+        }
 
+    }
+
+    public void onBuild(View v) {
+        sendNormalNotification();
+        sendFoldNotification();
+        sendHangNotification();
+        test();
+    }
+
+    private void test() {
+        int NOTIFICATION_ID = 234;
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context
+                .NOTIFICATION_SERVICE);
+
+
+        String CHANNEL_ID = "my_channel_01";
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            CharSequence name = "my_channel";
+            String Description = "This is my channel";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            mChannel.setDescription(Description);
+            mChannel.enableLights(true);
+            mChannel.setLightColor(Color.RED);
+            mChannel.enableVibration(true);
+            mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            mChannel.setShowBadge(false);
+            notificationManager.createNotificationChannel(mChannel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Title")
+                .setContentText("message");
+
+        Intent resultIntent = new Intent(this, MainActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(MainActivity.class);
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent
+                .FLAG_UPDATE_CURRENT);
+
+        builder.setContentIntent(resultPendingIntent);
+
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    private void sendFoldNotification() {
         Notification.Builder builder = new Notification.Builder(this);
         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/jitash"));
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
         builder.setContentIntent(pendingIntent);
-
-
-        sendNormalNotification(builder);
-        sendFoldNotification(builder);
-    }
-
-    private void sendFoldNotification(Notification.Builder builder) {
         RemoteViews remoteViews = new RemoteViews(getPackageName(), R.layout.view_fold);
         builder.setSmallIcon(R.drawable.foldleft);
         builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable
@@ -51,12 +117,37 @@ public class NofiticationActivity extends AppCompatActivity {
         }
     }
 
-    private void sendNormalNotification(Notification.Builder builder) {
+    private void sendNormalNotification() {
+        Notification.Builder builder = new Notification.Builder(this);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/jitash"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        builder.setContentIntent(pendingIntent);
         builder.setSmallIcon(R.drawable.ic_launcher_background);
         builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable
                 .ic_launcher_background));
         builder.setAutoCancel(true);
         builder.setContentTitle("普通通知");
         mNotificationManager.notify(0, builder.build());
+    }
+
+    private void sendHangNotification() {
+        Notification.Builder builder = new Notification.Builder(this);
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://jitash.cn"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        builder.setContentIntent(pendingIntent);
+        builder.setSmallIcon(R.drawable.foldleft);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable
+                .ic_launcher_background));
+        builder.setAutoCancel(true);
+        builder.setContentTitle("悬挂式通知");
+        //设置点击跳转
+        Intent hangIntent = new Intent();
+        hangIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        hangIntent.setClass(this, MainActivity.class);
+        PendingIntent hangPendingIntent = PendingIntent.getActivity(this, 0, hangIntent,
+                PendingIntent.FLAG_CANCEL_CURRENT);
+        builder.setFullScreenIntent(hangPendingIntent, true);
+        mNotificationManager.notify(2, builder.build());
+
     }
 }
